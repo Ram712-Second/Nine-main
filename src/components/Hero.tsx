2ed7d6dcb12c412a732ded708245595f0823c09e
Ram712-Second <ramduthrajesh4@gmail.com>
2026-06-14 12:43:03 +0530

    Replace duplicate hero image with new image path

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -13,7 +13,7 @@ import Autoplay from 'embla-carousel-autoplay';
 const heroImages = [
   '/web-images/hero1_1.jpg',
   '/web-images/hero1_2.jpg',
-  '/web-images/hero1_2.jpg',
+  '/projects/Hospital/SANJIVANI-HOSPITAL/sanjivani-hospital.png',
 ];
 
 const headlines = [
